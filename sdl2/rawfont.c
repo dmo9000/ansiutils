d12fe91d8a4160b8ef069cab0c23e87deb90dae2
Dan Morriss <daniel_morriss@yahoo.com.au>
2018-03-26 21:44:24 +1100

    Start to modularize.

--- sdl2/rawfont.c
+++ sdl2/rawfont.c
@@ -9,25 +9,45 @@
 #define CANVAS_WIDTH    80
 #define CANVAS_HEIGHT   24
 
+BitmapFont *bmf_load(char *filename);
+
 extern int gfx_main(uint16_t, uint16_t);
 extern int gfx_drawglyph(BitmapFont *bmf, uint8_t px, uint8_t py, uint8_t glyph);
 extern int gfx_expose();
 
 int main(int argc, char *argv[])
 {
-    size_t font_len_expect;
-    uint8_t *fontdata = NULL;
-    FILE *rawfont = NULL;
     char *filename = (char *) argv[1];
-    uint8_t height = 8;
-    uint8_t width = 8;
-    uint16_t rd = 0, r = 0;
-    BitmapFont myfont;
+    BitmapFont *myfont;
+    filename = "bmf/8x8.bmf";
 
-    if (argc < 2) {
-            printf("*** using default font ***\n");
-            filename = "bmf/8x8.bmf";
+    myfont = bmf_load(filename);
+
+    if (!myfont) {
+            perror("bmf_load");
+            exit(1);
             }
+
+    gfx_main((CANVAS_WIDTH*8), (CANVAS_HEIGHT*16));
+    for (int kk = 0; kk < 256; kk++) {
+        gfx_drawglyph(myfont, (kk % CANVAS_WIDTH), (kk / CANVAS_WIDTH), kk);
+        }
+    gfx_expose();
+    while (!getchar()) {
+    }
+    exit(0);
+
+}
+
+
+BitmapFont *bmf_load(char *filename)
+{
+
+    FILE *rawfont = NULL;
+    size_t font_len_expect;
+    BitmapFont *myfont = NULL;
+    int r = 0;
+    int rd = 0;
 
     rawfont = fopen(filename, "rb");
     if (!rawfont) {
@@ -40,63 +60,61 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    r = fread(&myfont.header, sizeof(BitmapFontHeader), 1, rawfont);
+    myfont = (BitmapFont*) malloc(sizeof(BitmapFont));
+
+    r = fread(&myfont->header, sizeof(BitmapFontHeader), 1, rawfont);
 
     if (r != 1) {
         perror("fread() header");
         exit(1);
     }
 
-    printf("Magic:   [%c%c%c]\n", myfont.header.magic[0], myfont.header.magic[1], myfont.header.magic[2]);
-    printf("Version: [%u]\n", myfont.header.version);
-    printf("Columns: [%u]\n", myfont.header.px);
-    printf("Rows:    [%u]\n", myfont.header.py);
-    printf("Glyphs:  [%u]\n", myfont.header.glyphs);
+    printf("Magic:   [%c%c%c]\n", myfont->header.magic[0], myfont->header.magic[1], myfont->header.magic[2]);
+    printf("Version: [%u]\n", myfont->header.version);
+    printf("Columns: [%u]\n", myfont->header.px);
+    printf("Rows:    [%u]\n", myfont->header.py);
+    printf("Glyphs:  [%u]\n", myfont->header.glyphs);
 
-    assert(myfont.header.magic[0] == 'B');
-    assert(myfont.header.magic[1] == 'M');
-    assert(myfont.header.magic[2] == 'F');
-    assert(myfont.header.version == 0);
-    assert(myfont.header.px == 8);
-    assert(myfont.header.py == 8);
-    assert(myfont.header.glyphs == 256);
+    assert(myfont->header.magic[0] == 'B');
+    assert(myfont->header.magic[1] == 'M');
+    assert(myfont->header.magic[2] == 'F');
+    assert(myfont->header.version == 0);
+    assert(myfont->header.px == 8);
+    assert(myfont->header.py == 8);
+    assert(myfont->header.glyphs == 256);
 
-    font_len_expect = (myfont.header.py * myfont.header.glyphs);
+    font_len_expect = (myfont->header.py * myfont->header.glyphs);
 
     assert(fseek(rawfont, 0, SEEK_END) == 0);
-    myfont.size = ftell(rawfont) - sizeof(BitmapFontHeader);
-    assert ((bool) (myfont.size == font_len_expect));
-    myfont.fontdata = malloc(myfont.size);
+    myfont->size = ftell(rawfont) - sizeof(BitmapFontHeader);
+    assert ((bool) (myfont->size == font_len_expect));
+    myfont->fontdata = malloc(myfont->size);
 
-    if (!myfont.fontdata) {
+    if (!myfont->fontdata) {
         perror("malloc");
         exit(1);
     }
-    printf("expected and found %lu bytes\n", (myfont.size));
+
+
+    printf("expected and found %lu bytes\n", (myfont->size));
 
     assert(fseek(rawfont, sizeof(BitmapFontHeader), SEEK_SET) == 0);
 
     while (rd < font_len_expect) {
-        r = fread(myfont.fontdata + rd, width*height, 1, rawfont);
+        r = fread(myfont->fontdata + rd, myfont->header.py, 1, rawfont);
         if (r != 1) {
             perror("fread");
-            for (int kk = 0; kk < width*height; kk++) {
-                printf("%02x ", fontdata[rd+kk]);
+            printf("r = %u\n", r);
+            for (int kk = 0; kk < myfont->header.px*myfont->header.py; kk++) {
+                printf("%02x ", myfont->fontdata[rd+kk]);
             }
             exit(1);
         }
-        rd += (r * (width*height));
+        rd += (r * (myfont->header.py));
     }
 
     printf("read %lu bytes\n", (long unsigned int) rd);
     fclose(rawfont);
-    gfx_main((CANVAS_WIDTH*8), (CANVAS_HEIGHT*16));
-    for (int kk = 0; kk < 256; kk++) {
-        gfx_drawglyph(&myfont, (kk % CANVAS_WIDTH), (kk / CANVAS_WIDTH), kk);
-        }
-    gfx_expose();
-    while (!getchar()) {
-    }
-    exit(0);
 
+    return (myfont);
 }
