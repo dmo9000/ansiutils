3b527f30ab74de8971017cf574bfe55c21894c39
Dan Morriss <daniel_morriss@yahoo.com.au>
2018-03-26 18:01:22 +1100

    Updates.

--- vgafonts/rawfont.c
+++ vgafonts/rawfont.c
@@ -4,73 +4,93 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <errno.h>
+#include "rawfont.h"
 
 #define MAX_CHARS 256
 
 extern int gfx_main(uint16_t, uint16_t);
 extern int gfx_drawglyph(uint8_t *fontdata, uint8_t height, uint16_t px, uint16_t py, uint16_t glyph);
 
+
 int main(int argc, char *argv[])
 {
-	size_t font_len_expect;
-	size_t font_len_actual;
-	uint8_t *fontdata = NULL;
-	FILE *rawfont = NULL;
-	char *filename = (char *) argv[1];
-	uint8_t height = 8;
-	uint8_t width = 8;
-	uint16_t rd = 0, r = 0;
+    size_t font_len_expect;
+    size_t font_len_actual;
+    uint8_t *fontdata = NULL;
+    FILE *rawfont = NULL;
+    char *filename = (char *) argv[1];
+    uint8_t height = 8;
+    uint8_t width = 8;
+    uint16_t rd = 0, r = 0;
+    BitmapFont myfont;
 
-	/* hardcoded for 8x8 font as first argument for now */
+    /* hardcoded for 8x8 font as first argument for now */
 
-	font_len_expect = (width * height * MAX_CHARS);
-	rawfont = fopen(filename, "rb");
-	if (!rawfont) {
-		perror("fopen");
-		exit(1);
-		}
+    font_len_expect = (width * height * MAX_CHARS);
+    rawfont = fopen(filename, "rb");
+    if (!rawfont) {
+        perror("fopen");
+        exit(1);
+    }
 
-	if (fseek(rawfont, 0, SEEK_END)) {
-			perror("fseek");
-			exit(1);
-			}
+    if (fseek(rawfont, 0, SEEK_SET)) {
+        perror("fseek");
+        exit(1);
+    }
 
-	font_len_actual = ftell(rawfont);
+    r = fread(&myfont, sizeof(BitmapFont), 1, rawfont);
 
-	assert ((bool) (font_len_actual == font_len_expect));
+    if (r != 1) {
+        perror("fread() header");
+        exit(1);
+    }
 
-	fontdata = malloc(font_len_expect);
+    printf("Magic:   [%c%c%c]\n", myfont.magic[0], myfont.magic[1], myfont.magic[2]);
+    printf("Version: [%u]\n", myfont.version);
+    printf("Columns: [%u]\n", myfont.px);
+    printf("Rows:    [%u]\n", myfont.py);
+    printf("Glyphs:  [%u]\n", myfont.glyphs);
 
-	if (!fontdata) {
-			perror("malloc");
-			exit(1);
-			}
-	printf("expected and found %lu bytes\n", (font_len_expect));
+    assert(myfont.version == 0);
+    assert(myfont.px == 8);
+    assert(myfont.py == 8);
+    assert(myfont.glyphs == 256);
 
-	while (rd < font_len_expect) {
-			printf("rd = %u, reading %u\n", rd, width*height);
-			r = fread(fontdata + rd, 1, width*height, rawfont);
-			if (!r) {
-					perror("fread");
-					for (int kk = 0; kk < width*height; kk++) {
-						printf("%02x ", fontdata[rd+kk]);
-						}
-					exit(1);
-					}
-			printf("r = %u\n", r);
-			rd += (r * (width*height));
-			}
+    font_len_actual = ftell(rawfont);
+    assert ((bool) (font_len_actual == font_len_expect));
 
-	printf("read %lu bytes\n", (long unsigned int) rd);
+    fontdata = malloc(font_len_expect);
 
-	fclose(rawfont);
+    if (!fontdata) {
+        perror("malloc");
+        exit(1);
+    }
+    printf("expected and found %lu bytes\n", (font_len_expect));
 
-  gfx_main(height * (16*2), width * (16*2));
+    while (rd < font_len_expect) {
+        printf("rd = %u, reading %u\n", rd, width*height);
+        r = fread(fontdata + rd, 1, width*height, rawfont);
+        if (!r) {
+            perror("fread");
+            for (int kk = 0; kk < width*height; kk++) {
+                printf("%02x ", fontdata[rd+kk]);
+            }
+            exit(1);
+        }
+        printf("r = %u\n", r);
+        rd += (r * (width*height));
+    }
 
-	gfx_drawglyph(fontdata, height, 0, 0, 'A');
+    printf("read %lu bytes\n", (long unsigned int) rd);
 
-	while (!getchar()) {
-		}
-	exit(0);
+    fclose(rawfont);
+
+    gfx_main(height * (16*2), width * (16*2));
+
+    gfx_drawglyph(fontdata, height, 0, 0, 'A');
+
+    while (!getchar()) {
+    }
+    exit(0);
 
 }
---
+++ vgafonts/rawfont.h
@@ -0,0 +1,14 @@
+
+
+struct bitmapfont {
+                    unsigned char magic[3];
+                    uint8_t version;
+                    uint8_t px;
+                    uint8_t py;
+                    uint16_t glyphs;
+                  };
+
+typedef struct bitmapfont BitmapFont;
+
+
+
