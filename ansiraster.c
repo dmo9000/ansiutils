9bc6fcc05346aaf2d9ec0823305187f2284f48b6
Dan Morriss <daniel.morriss@visionstream.com.au>
2018-03-29 12:29:50 +1100

    Seperate raster resize code from raster_append_byte().

--- ansiraster.c
+++ ansiraster.c
@@ -24,7 +24,6 @@ bool raster_append_bytes(ANSIRaster *r, unsigned char *data, uint8_t bytes, ansi
 {
 
     int ii = 0;
-
     /* uncomment this assert to filter some dogdy fonts, need to know why though */
 
     for (ii = 0; ii < bytes; ii++) {
@@ -36,13 +35,77 @@ bool raster_append_bytes(ANSIRaster *r, unsigned char *data, uint8_t bytes, ansi
 
 }
 
-bool raster_append_byte(ANSIRaster *r, unsigned char data, ansicolor_t fg, ansicolor_t bg, bool debug)
+bool raster_resize(ANSIRaster *r, uint16_t size)
 {
-    ANSIRaster *tdr = r;
     unsigned char *chardata_realloc = NULL;
     unsigned char *fgcolors_realloc = NULL;
     unsigned char *bgcolors_realloc = NULL;
 
+    /* raster must be valid */
+    assert(r);
+    /* must request resize by at least one byte */
+    assert(size);
+    /* size must be only a single byte (for now) */
+    assert(size == r->bytes + 1);
+    /* new size must be larger than old size */
+    assert(size > r->bytes);
+    /* new size must not cause raster to exceed maximum length */
+    assert(size + r->bytes <= 65535);
+
+    /* if the raster is empty, allocate an initial byte */
+
+    if (!r->bytes) {
+        /* if the raster is empty, allocate an initial byte */
+        r->bytes = 1;
+        /* TODO - since we're planning not to just printf() anymore,  probably no need to add the extra byte for much longer */
+        r->chardata = malloc(2);
+        r->fgcolors = malloc(2);
+        r->bgcolors = malloc(2);
+        r->chardata[0] = 0;
+        r->fgcolors[0] = 7;
+        r->bgcolors[0] = 0;
+        r->chardata[1] = '\0'; /* NULL terminate, so that the rasters can be printed with C library functions */
+        r->fgcolors[1] = '\0';
+        r->bgcolors[1] = '\0';
+        r->bytes = 1;
+        return true;
+    }
+
+    /* otherwise we are resizing an existing raster */
+    /* ensure we already have raster data */
+    assert(r->chardata);
+    /* ensure we already have foreground color data */
+    assert(r->fgcolors);
+    /* ensure we already have background color data */
+    assert(r->bgcolors);
+    /* assert that the raster already has a length greater than 0 */
+    assert(r->bytes);
+
+    r->bytes ++;
+    chardata_realloc = realloc(r->chardata, r->bytes+1);
+    fgcolors_realloc = realloc(r->fgcolors, r->bytes+1);
+    bgcolors_realloc = realloc(r->bgcolors, r->bytes+1);
+
+    /* ensure realloc() didn't fail */
+    assert(chardata_realloc);
+    assert(fgcolors_realloc);
+    assert(bgcolors_realloc);
+
+    /* assign the realloc'd regions to the relevant pointers */
+    r->chardata = chardata_realloc;
+    r->fgcolors = fgcolors_realloc;
+    r->bgcolors = bgcolors_realloc;
+    /* ensure the new raster does not have length 0 */
+    assert(r->bytes-1);
+
+    return true;
+}
+
+bool raster_append_byte(ANSIRaster *r, unsigned char data, ansicolor_t fg, ansicolor_t bg, bool debug)
+{
+    ANSIRaster *tdr = r;
+
+    /* raster must exist */
     assert(r);
     /* do not push null byte, ever, since it's a string terminator  */
 
@@ -54,14 +117,8 @@ bool raster_append_byte(ANSIRaster *r, unsigned char data, ansicolor_t fg, ansic
     assert(data);
 
     if (!tdr->bytes) {
-        tdr->bytes = 1;
         /* TODO - since we're planning not to just printf() anymore,  probably no need to add the extra byte for much longer */
-        tdr->chardata = malloc(tdr->bytes+1);
-        tdr->fgcolors = malloc(tdr->bytes+1);
-        tdr->bgcolors = malloc(tdr->bytes+1);
-
-        /* write the byte and its foreground and background colors */
-
+        raster_resize(tdr, tdr->bytes+1);
         tdr->chardata[0] = data;
         tdr->fgcolors[0] = fg;
         tdr->bgcolors[0] = bg;
@@ -70,26 +127,7 @@ bool raster_append_byte(ANSIRaster *r, unsigned char data, ansicolor_t fg, ansic
         tdr->bgcolors[1] = '\0';
         return true;
     } else {
-        assert(tdr->chardata);
-        assert(tdr->fgcolors);
-        assert(tdr->bgcolors);
-        assert(tdr->bytes);
-        tdr->bytes ++;
-        if (debug) {
-            //    printf("realloc(), tdr->bytes = %d, address = 0x%08x\n", tdr->bytes+1, tdr->chardata);
-        }
-        chardata_realloc = realloc(tdr->chardata, tdr->bytes+1);
-        fgcolors_realloc = realloc(tdr->fgcolors, tdr->bytes+1);
-        bgcolors_realloc = realloc(tdr->bgcolors, tdr->bytes+1);
-
-        assert(chardata_realloc);
-        assert(fgcolors_realloc);
-        assert(bgcolors_realloc);
-
-        tdr->chardata = chardata_realloc;
-        tdr->fgcolors = fgcolors_realloc;
-        tdr->bgcolors = bgcolors_realloc;
-        assert(tdr->bytes-1);
+        raster_resize(tdr, tdr->bytes+1);
         tdr->chardata[tdr->bytes-1] = data;
         tdr->fgcolors[tdr->bytes-1] = fg;
         tdr->bgcolors[tdr->bytes-1] = bg;
