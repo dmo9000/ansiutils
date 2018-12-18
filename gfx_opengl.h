25bf52169a0b73e1df8b3ddec55cdc825c6772f4
Dan Morriss <daniel_morriss@yahoo.com.au>
2018-12-18 16:15:50 +1100

    Add some missing prototypes.

--- gfx_opengl.h
+++ gfx_opengl.h
@@ -2,6 +2,10 @@
 #include <stdbool.h>
 #include "rawfont.h"
 
+uint16_t gfx_opengl_getwidth();
+uint16_t gfx_opengl_getheight();
+void gfx_opengl_setdimensions(uint16_t w, uint16_t h);
+
 int gfx_opengl_main(uint16_t, uint16_t, int modifier, char *WindowTitle);
 int gfx_opengl_drawglyph(BitmapFont *bmf, uint16_t px, uint16_t py, uint8_t glyph, uint8_t fg, uint8_t bg, uint8_t attr);
 int gfx_opengl_expose();
