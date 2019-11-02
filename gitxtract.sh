339d371da18a1ad1c20814aa0ab900f492e26a5b
Dan Morriss <daniel.morriss@visionstream.com.au>
2019-11-02 15:45:54 +1100

    Fixups to git summaries.

--- gitxtract.sh
+++ gitxtract.sh
@@ -1,7 +1,10 @@
 SUMMARY_COUNT=5
+
+# get SUMMARY_COUNT last commits
+
 SUMMARY_TAGS=`git rev-list HEAD | head -n ${SUMMARY_COUNT}` 
 
-FMT=`which gfmt`
+FMT=`which gfmt 2>/dev/null`
 # On FreeBSD, all GNU utils from coreutils have a 'g' prefix
 if [ -z "${FMT}" ]; then
 	FMT=`which fmt`
@@ -9,8 +12,12 @@ if [ -z "${FMT}" ]; then
 
 for COMMIT in $SUMMARY_TAGS; do 
 	SHORTHASH=`echo ${COMMIT} | cut -b1-7`
-	COMMENT=`git log --no-walk ${COMMIT} | grep "^ " | sed "s/^\ */\* /" | fold -s -w 90 | ${FMT} -t | sed "s/^  /         /g"`
-	echo "${SHORTHASH} ${COMMENT}" | sed "s/^\*/        \*/" 
+	#COMMENT=`git log --no-walk ${COMMIT} | grep "^ " | sed "s/^\ */\* /" | fold -s -w 90 | ${FMT} -t | sed "s/^  /         /g"`
+	COMMENT=`git log --no-walk ${COMMIT} | grep "^ " | paste -s -d " " | tr -s " " | sed -e "s/^\ *//" | fmt -w 90 | fmt -w 90 -t | sed '/^*\ /! s/\(.*\)/\^\1/'`
+
+	ANSI_FG_RED=`./ansitext FG_RED`
+	ANSI_FG_NONE=`./ansitext FG_NONE`
+	echo "${ANSI_FG_RED}${SHORTHASH}${ANSI_FG_NONE} ${COMMENT}" | sed "s/^\^/          /" | sed "s/\^/\* /g" 
 	done
 
 
