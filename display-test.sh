465fbfd1acb2a247f3314c72e7247a0b64fd9314
Dan Morriss <daniel.morriss@visionstream.com.au>
2019-11-09 13:35:03 +1100

    Visual results audit; about 1000+ working, around 90 not rendering correctly.

--- display-test.sh
+++ display-test.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+echo "" > visual-results.csv
+
 for ANS in `ls -1 tests/pass/utf8/*.ans`; do
     echo $ANS
     cat $ANS
+		ANSFILE=`basename $ANS`
     echo ""
+		echo "Rendered correctly? [y/n]"
     read INPUT
-    done
+
+		case ${INPUT} in
+        [yY] | [yY][Ee][Ss] )
+								echo "Y,${ANSFILE}" >> visual-results.csv
+                ;;
+        [nN] | [n|N][O|o] )
+								echo "N,${ANSFILE}" >> visual-results.csv
+                ;;
+        *) echo "Invalid input"
+            ;;
+		esac
+done
+
 
